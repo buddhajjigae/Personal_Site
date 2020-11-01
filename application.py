24ceed55ba5de5da0b00ffff7498683e5197a92f
Alex Yu <72051877+buddhajjigae@users.noreply.github.com>
2020-10-31 22:23:34 -0400

    Update application.py
    Added connection to AWS RDS MySQL server
    Created function getQuote() that routes to /quote and selects a random quote and author from database. 
    To do: Json format the quote

--- application.py
+++ application.py
@@ -12,15 +12,22 @@ sys.path.append(cwd)
 
 #print("*** PYHTHONPATH = " + str(sys.path) + "***")
 
+import json
 import logging
 from datetime import datetime
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, Response, url_for
+from flask_sqlalchemy import SQLAlchemy
+#from flask_marshmallow import Marshmallow
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
 application = Flask(__name__)
+application.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://user:pw@url/dbname'
+db = SQLAlchemy(application)
+#ma = Marshmallow(application)
+
 
 # 1. Extract the input information from the requests object.
 # 2. Log the information
@@ -73,6 +80,29 @@ def log_response(method, status, data, txt):
 def index():
     return render_template('index.html')
 
+@application.route("/quote", methods=["GET"])
+def getQuote():
+    #sql = "select author from quotes"
+    sql = "SELECT author, quote FROM quotes ORDER BY RAND() LIMIT 1"
+    ranQuote = []
+    result = db.engine.execute(sql)
+    #author = [row for row in result]
+    #quote = [row for row in result]
+
+    x = '{ "author":"John", "quote":"New York"}'
+    for row in result:
+        ranQuote.append(row)
+    #rsp = Response("Hello World", status=200, content_type="text/plain")
+    #print(author)
+    #print(quote)
+    #rsp = Response(ranQuote[0], status=200, content_type="text/plain")
+    rsp = Response( json.dumps(x) , status=200, content_type="application/json")
+    return rsp
+
+getQuote()
+
+
 # Run the app.
 if __name__ == "__main__":
+    db.init_app(application)
     application.run(port=8000)
